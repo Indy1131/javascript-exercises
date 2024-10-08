32a510a0e475c2b7a8b529e994bc7f01f3c85648
Indy1131 <62616129+Indy1131@users.noreply.github.com>
2024-10-07 23:18:45 -0400

    completed exercies 8-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  findTheOldest(people);
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
